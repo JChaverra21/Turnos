fc200f68060953f0ca04c69412ff4d7ad5a8b50f
Jose <david-restrepo_123@hotmail.es>
2022-12-19 12:19:19 -0500

    Se crea el controlador Especialidad y la vista Especialidad (Index)

---
+++ Controllers/EspecialidadController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turnos.Controllers
+{
+    public class EspecialidadController : Controller
+    {
+        //Constructor
+        public EspecialidadController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
